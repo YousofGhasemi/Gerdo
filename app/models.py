8e513aa467de93d7fa3328d76509339784799a88
PC-Yousof <yosef.ghasemi.all@gmail.com>
2025-10-31 14:49:35 +0330

    basic models for users and currencies and boxes is created.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, primary_key=True
+    )
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=48)
+
+    def __str__(self):
+        return self.name
+
+
+class Customers(models.Model):
+    name = models.CharField(max_length=50)
+    debt = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Currency(models.Model):
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    decimal_point = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+    )
+
+    def __str__(self):
+        return self.code
+
+
+class Box(models.Model):
+    name = models.CharField(max_length=50)
+    data = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        return self.name
